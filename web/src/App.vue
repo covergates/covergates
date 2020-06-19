79ebfaf157e0d5bf8e40bb56831c3b31cbfd60ca
blueworrybear <blueworrybear@gmail.com>
2020-06-19 16:32:53 +0000

    init web app

---
+++ web/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app id="inspire">
+    <v-app id="keep">
+      <v-app-bar
+        app
+        color="primary"
+        dark
+        clipped-left
+      >
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-spacer></v-spacer>
+
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        clipped
+        app
+      >
+      </v-navigation-drawer>
+
+      <v-main>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </v-main>
+    </v-app>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class AppComponent extends Vue {
+  private drawer = true
+}
+
+</script>
---
+++ web/src/main.ts
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// External Libs
+import axios from 'axios'
+import vuetify from './plugins/vuetify'
+
+Vue.config.productionTip = false
+Vue.prototype.$http = axios
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
